2c6be2a135a9f1cd36ecf27dc43cc6b082f8fae1
Milo Wical <Milo.Wical@compare.com>
2016-09-23 20:34:59 -0400

    Updated the install file to allow for multiple versions.

--- src/Install.ps1
+++ src/Install.ps1
@@ -1,5 +1,8 @@
 #Create an unsecured local development cluster
 
-cd C:\ServiceFabric\5.1.156.9590
+#$Version = "5.1.156.9590"
+$Version = "5.3.124.9494"
+
+cd C:\ServiceFabric\$Version
 
 .\CreateServiceFabricCluster.ps1 -ClusterConfigFilePath .\ClusterConfig.Unsecure.DevCluster.json -MicrosoftServiceFabricCabFilePath .\MicrosoftAzureServiceFabric.cab -AcceptEULA $true
